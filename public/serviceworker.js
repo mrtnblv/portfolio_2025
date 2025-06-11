b864386edeffbe727503fba917b618bc231e7f4f
Martin B Levy (via WeWeb) <weweb@martinblevy.com>
2025-06-11 09:14:58 +0200

    v54 - Remove animation logo for playground + alt on tools' logo

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,4 +1,4 @@
-const version = 53;
+const version = 54;
 self.addEventListener('install', event => {
     // eslint-disable-next-line no-console
     console.log(`Service worker v${version} installed`);
