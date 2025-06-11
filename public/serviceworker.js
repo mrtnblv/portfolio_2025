240cc89411fc35dc9c72d19e7c21e1000bf719c6
Martin B Levy (via WeWeb) <weweb@martinblevy.com>
2025-06-11 09:21:35 +0200

    v55 - Remove randomizing projects / Fix randomizing tools

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,4 +1,4 @@
-const version = 54;
+const version = 55;
 self.addEventListener('install', event => {
     // eslint-disable-next-line no-console
     console.log(`Service worker v${version} installed`);
